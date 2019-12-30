fbc3215043d0cc2eccefc32eadd36be541585034
Ryan <naamloosdt@gmail.com>
2019-12-30 23:55:56 +0100

    Increase version

--- src/Program.cs
+++ src/Program.cs
@@ -66,7 +66,7 @@ namespace BGEngine
             Application.Run(new MainWindow());
         }
 
-        const string EXPECTED_GH_VERSION_TAG = "0.3BETA";
+        const string EXPECTED_GH_VERSION_TAG = "0.4BETA";
         static void CheckForUpdates()
         {
             try
